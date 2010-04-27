4e0bb4238ec947eda7389c78aacbb562079d6792
Curtis (Programble) <programble@gmail.com>
2010-04-26 20:44:10 -0400

    Edited MOTD

--- config.py
+++ config.py
@@ -11,5 +11,11 @@ motd = """                                         __               __
 
                              omgircd testing server
                              
+                               Official Channels:
+                          #test - Main testing channel
+                    #moo - Spam testing channel and cow farm
+                             
+                     Server Admin: curtis/programble/urtis
+                             
                      WARNING: This server is very unstable
 """
